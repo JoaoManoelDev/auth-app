d1c1b7651c60b4d7d50689b5ba7b332114becd6d
João <joaomanoeldev@gmail.com>
2023-01-07 20:12:46 -0300

    Configuring ESLint and StichesJS

---
+++ src/pages/_document.page.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from '../styles'
+
+export default function Document() {
+  return (
+    <Html lang="pt-br">
+      <Head>
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { globalCss } from '.'
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  body: {
+    '-webkit-font-smoothing': 'antialised',
+    backgroundColor: '$gray900',
+    color: '$gray100',
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+  },
+
+  button: {
+    cursor: 'pointer',
+  },
+})
