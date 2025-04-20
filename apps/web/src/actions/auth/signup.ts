fb2987595d1888845fd6026a91715202e80b0265
João Manoel <joaomanoeldev@gmail.com>
2025-04-20 17:46:42 -0300

    feat: create signup action

---
+++ apps/web/src/actions/auth/signup.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import { redirect } from "next/navigation";
+
+// export type SignupFormState =
+//   | {
+//       errors?: {
+//         name?: string[];
+//         email?: string[];
+//         password?: string[];
+//       };
+//       message?: string;
+//     }
+//   | undefined;
+
+interface SignupRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface SignupResponse {
+  isError: boolean;
+  dataError: {
+    status: number;
+    message: string;
+    erros?: {
+      field: string;
+      message: string[];
+    }[];
+  };
+}
+
+export const signup = async (
+  signupData: SignupRequest
+): Promise<SignupResponse> => {
+  const response = await fetch("http://localhost:3333/auth/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(signupData),
+  });
+
+  if (response.ok) {
+    return redirect("/signin");
+  }
+
+  return {
+    isError: true,
+    dataError: await response.json(),
+  };
+};
