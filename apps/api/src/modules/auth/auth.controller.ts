66f20b9d985ee9924abfdf5945fc9cc09ee2a259
João Manoel <joaomanoeldev@gmail.com>
2025-04-21 22:10:50 -0300

    feat: create signin route

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { ValidationPipe } from "src/pipes/class-validator-pipe";
 import { CreateUserDTO } from "src/modules/user/dto/create-user.dto";
+import { SigninDTO } from "src/modules/auth/dto/signin-dto";
 
 @Controller("/auth")
 export class AuthController {
@@ -12,4 +13,7 @@ export class AuthController {
   async signup(@Body(new ValidationPipe()) createUserDTO: CreateUserDTO) {
     await this.authService.registerUser(createUserDTO);
   }
+
+  @Post("/signin")
+  async signin(@Body(new ValidationPipe()) signinDTO: SigninDTO) {}
 }
