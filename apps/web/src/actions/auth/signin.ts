27baee1afa9b79a5292a80d85c172e6577dca3ac
João Manoel <joaomanoeldev@gmail.com>
2025-04-22 00:21:37 -0300

    feat: add signin action

---
+++ apps/web/src/actions/auth/signin.ts
@@ -0,0 +1,37 @@
+interface SigninRequest {
+  email: string;
+  password: string;
+}
+
+interface SigninResponse {
+  isError: boolean;
+  dataError: {
+    status: number;
+    message?: string;
+    name?: string;
+  } | null;
+}
+
+export const signin = async (
+  signinData: SigninRequest
+): Promise<SigninResponse> => {
+  const response = await fetch("http://localhost:3333/auth/signin", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(signinData),
+  });
+
+  if (response.status === 201) {
+    return {
+      isError: false,
+      dataError: await response.json(),
+    };
+  }
+
+  return {
+    isError: true,
+    dataError: null,
+  };
+};
