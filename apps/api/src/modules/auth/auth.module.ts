4ad6f3d2396a0a2363596cdb7987dbbfe5f2d6d9
João Manoel <joaomanoeldev@gmail.com>
2025-04-21 23:34:37 -0300

    chore: add LocalStrategy in module

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -6,9 +6,10 @@ import { SignupController } from "./controllers/signup.controller";
 import { AuthService } from "./auth.service";
 import { UserService } from "src/modules/user/user.service";
 import { PrismaService } from "src/prisma/prisma.service";
+import { LocalStrategy } from "./strategies/local.strategy";
 
 @Module({
   controllers: [SigninController, SignupController],
-  providers: [AuthService, UserService, PrismaService],
+  providers: [AuthService, UserService, PrismaService, LocalStrategy],
 })
 export class AuthModule {}
