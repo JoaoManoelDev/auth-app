cf7298ef6fb0c3d1933b9fbddbb9682be6b91b65
João Manoel <joaomanoeldev@gmail.com>
2025-04-22 00:21:09 -0300

    chore: add cors config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: "*",
+  });
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
