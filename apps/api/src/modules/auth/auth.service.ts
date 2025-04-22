c1f15e0e9ff4e4e518e9645de24852914e9adb17
João Manoel <joaomanoeldev@gmail.com>
2025-04-21 22:10:24 -0300

    feat: create validate local user method

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { compare } from "bcryptjs";
 
 import { CreateUserDTO } from "src/modules/user/dto/create-user.dto";
 import { UserService } from "src/modules/user/user.service";
@@ -17,5 +22,30 @@ export class AuthService {
     }
 
     return this.userService.create(createUserDTO);
+  }
+
+  async validateLocalUser({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) {
+    const user = await this.userService.findByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException("invalid_credentials");
+    }
+
+    const isPassowordMatched = await compare(password, user.password);
+
+    if (!isPassowordMatched) {
+      throw new UnauthorizedException("invalid_credentials");
+    }
+
+    return {
+      id: user.id,
+      name: user.name,
+    };
   }
 }
