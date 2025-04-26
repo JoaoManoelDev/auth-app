1ef580aba412fad66e8625eb851b8c94d02b3542
João Manoel <joaomanoeldev@gmail.com>
2025-04-26 17:41:42 -0300

    feat: redirects the user if he is already logged in

--- apps/web/src/app/(auth)/signin/page.tsx
+++ apps/web/src/app/(auth)/signin/page.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 import { SigninForm } from "@/components/auth/signin-form";
+import { getSession } from "@/services/session";
 
-const SigninPage = () => {
+const SigninPage = async () => {
+  const session = await getSession();
+
+  if (session && session.user) return redirect("/");
+
   return (
     <div className="rounded-lg shadow-lg gap-8 w-sm flex flex-col justify-center items-center p-5">
       <h1 className="text-center text-2xl font-bold">Cadastre-se</h1>
--- apps/web/src/app/(auth)/signup/page.tsx
+++ apps/web/src/app/(auth)/signup/page.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 import { SignupForm } from "@/components/auth/signup-form";
+import { getSession } from "@/services/session";
 
-const SignupPage = () => {
+const SignupPage = async () => {
+  const session = await getSession();
+
+  if (session && session.user) return redirect("/");
+
   return (
     <div className="rounded-lg shadow-lg gap-8 w-sm flex flex-col justify-center items-center p-5">
       <h1 className="text-center text-2xl font-bold">Cadastre-se</h1>
