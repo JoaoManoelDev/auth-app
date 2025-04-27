c4e57c04e6524eb6d8b09222f91e9c428d604b88
João Manoel <joaomanoeldev@gmail.com>
2025-04-27 14:43:13 -0300

    chore: configure config module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 import { AuthModule } from "src/modules/auth/auth.module";
+import { envSchema } from "./env";
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
