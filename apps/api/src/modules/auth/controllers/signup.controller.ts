b00f1ee924ea2c01630d4b17bf1dc45646d3e478
João Manoel <joaomanoeldev@gmail.com>
2025-04-27 15:37:40 -0300

    refactor: remove auth service and add register user service

--- apps/api/src/modules/auth/controllers/signup.controller.ts
+++ apps/api/src/modules/auth/controllers/signup.controller.ts
@@ -2,14 +2,14 @@ import { Body, Controller, Post } from "@nestjs/common";
 
 import { ValidationPipe } from "src/pipes/class-validator-pipe";
 import { CreateUserDTO } from "src/modules/user/dto/create-user.dto";
-import { AuthService } from "../auth.service";
+import { RegisterUserService } from "../services/register-user.service";
 
 @Controller("/auth/signup")
 export class SignupController {
-  constructor(private authService: AuthService) {}
+  constructor(private registerUserService: RegisterUserService) {}
 
   @Post()
   async signup(@Body(new ValidationPipe()) createUserDTO: CreateUserDTO) {
-    await this.authService.registerUser(createUserDTO);
+    await this.registerUserService.handle(createUserDTO);
   }
 }
