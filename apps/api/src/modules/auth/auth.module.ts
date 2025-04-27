388c11c82595b5c01f3dc02263be3f9d559cc6e8
João Manoel <joaomanoeldev@gmail.com>
2025-04-27 14:44:18 -0300

    chore: configure jwt module

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 import { SigninController } from "./controllers/signin.controller";
 import { SignupController } from "./controllers/signup.controller";
@@ -6,10 +8,35 @@ import { SignupController } from "./controllers/signup.controller";
 import { AuthService } from "./auth.service";
 import { UserService } from "src/modules/user/user.service";
 import { PrismaService } from "src/prisma/prisma.service";
+import { LoginService } from "./services/login.service";
+import { GenerateTokensService } from "./services/generate-tokens.service";
 import { LocalStrategy } from "./strategies/local.strategy";
+import { Env } from "src/env";
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory: async (config: ConfigService<Env, true>) => {
+        const jwtSecret = config.get("JWT_SECRET", { infer: true });
+        const jwtExpiresIn = config.get("JWT_EXPIRES_IN", { infer: true });
+
+        return {
+          signOptions: { expiresIn: jwtExpiresIn },
+          secret: jwtSecret,
+        };
+      },
+    }),
+  ],
   controllers: [SigninController, SignupController],
-  providers: [AuthService, UserService, PrismaService, LocalStrategy],
+  providers: [
+    PrismaService,
+    LocalStrategy,
+    AuthService,
+    UserService,
+    LoginService,
+    GenerateTokensService,
+  ],
 })
 export class AuthModule {}
