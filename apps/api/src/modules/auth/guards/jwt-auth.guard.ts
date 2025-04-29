df0530650e982060878b89cc5f1420a293f2a3ce
João Manoel <joaomanoeldev@gmail.com>
2025-04-29 00:03:45 -0300

    feat: create jwt auth guard

---
+++ apps/api/src/modules/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,23 @@
+import { UnauthorizedException } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+
+interface ErrorMessage {
+  response: {
+    message: string;
+    error: string;
+    statusCode: string;
+  };
+}
+
+export class JWTAuthGuard extends AuthGuard("jwt") {
+  handleRequest<TUser>(err: ErrorMessage | null, user: TUser): TUser {
+    if (err || !user) {
+      throw new UnauthorizedException({
+        message: err?.response?.message ?? "unauthorized",
+        statusCode: 401,
+      });
+    }
+
+    return user;
+  }
+}
