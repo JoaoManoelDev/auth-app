218482782d179f2d6e5b62e4380c7c558b326567
João Manoel <joaomanoeldev@gmail.com>
2025-04-29 23:59:33 -0300

    feat: create get profile action

---
+++ apps/web/src/actions/users/get-profile.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { getSession } from "@/services/session";
+
+interface GetProfileResponseSuccess {
+  isError: false;
+  data: {
+    sub: string;
+  };
+}
+
+interface GetProfileResponseError {
+  isError: true;
+  data?: {
+    status?: number;
+    message?: string;
+    name?: string;
+  };
+}
+
+export const getProfile = async (): Promise<
+  GetProfileResponseSuccess | GetProfileResponseError
+> => {
+  const session = await getSession();
+
+  const response = await fetch(`http://localhost:3333/metrics`, {
+    headers: {
+      authorization: `Bearer ${session?.accessToken}`,
+    },
+  });
+
+  if (response.status === 200) {
+    const data = await response.json();
+
+    return {
+      isError: false,
+      data,
+    };
+  }
+
+  return {
+    isError: true,
+    data: await response.json(),
+  };
+};
