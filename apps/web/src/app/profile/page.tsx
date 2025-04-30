7cbce215d4513130aeb2f3dc840285f86e62827d
João Manoel <joaomanoeldev@gmail.com>
2025-04-29 23:59:45 -0300

    feat: add get profile action

--- apps/web/src/app/profile/page.tsx
+++ apps/web/src/app/profile/page.tsx
@@ -1,5 +1,18 @@
-const ProfilePage = () => {
-  return <div className="mt-20">Profile Page</div>;
+import { getProfile } from "@/actions/users/get-profile";
+
+const ProfilePage = async () => {
+  const response = await getProfile();
+
+  if (response.isError) {
+    return <div>Erro: {response.data?.message}</div>;
+  }
+
+  return (
+    <div className="mt-20">
+      <h1>Profile Page</h1>
+      <span>User ID: {response.data.sub}</span>
+    </div>
+  );
 };
 
 export default ProfilePage;
