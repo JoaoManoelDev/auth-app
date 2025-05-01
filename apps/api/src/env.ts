94b64431c202e288d53272dc54c1ffcf6be55fd3
João Manoel <joaomanoeldev@gmail.com>
2025-05-01 12:47:56 -0300

    chore: add REFRESH_JWT_SECRET and REFRESH_JWT_EXPIRES_IN envs in env schema

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -4,6 +4,8 @@ export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   JWT_SECRET: z.string(),
   JWT_EXPIRES_IN: z.string(),
+  REFRESH_JWT_SECRET: z.string(),
+  REFRESH_JWT_EXPIRES_IN: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
