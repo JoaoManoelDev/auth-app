46296b88c1c5f983c85f79b96034d1f71b1a53a1
João Manoel <joaomanoeldev@gmail.com>
2025-05-01 23:36:04 -0300

    refactor: remove unusable Body

--- apps/api/src/modules/auth/controllers/refresh-token.controller.ts
+++ apps/api/src/modules/auth/controllers/refresh-token.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Controller, Post, UseGuards } from "@nestjs/common";
 
 import { RefreshTokenGuard } from "../guards/refresh-token.guard";
 import { CurrentUser } from "../decorators/current-user-decorator";
