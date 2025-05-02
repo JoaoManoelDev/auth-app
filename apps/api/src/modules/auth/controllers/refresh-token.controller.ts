babf1e0259167102a52136534a86f2592d127a4c
João Manoel <joaomanoeldev@gmail.com>
2025-05-01 23:22:57 -0300

    feat: create refresh token controller

---
+++ apps/api/src/modules/auth/controllers/refresh-token.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+
+import { RefreshTokenGuard } from "../guards/refresh-token.guard";
+import { CurrentUser } from "../decorators/current-user-decorator";
+import { RefreshTokenService } from "../services/refresh-token.service";
+
+@Controller("/refresh-token")
+export class RefreshTokenController {
+  constructor(private refreshTokenService: RefreshTokenService) {}
+
+  @UseGuards(RefreshTokenGuard)
+  @Post()
+  async handle(@CurrentUser() user: { sub: string }) {
+    return await this.refreshTokenService.handle({ sub: user.sub });
+  }
+}
